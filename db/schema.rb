8a4db3b30efa574b097f6fbc4f1be0d1f6a583f9
stuartstein <stuart.stein@gmail.com>
2014-02-27 13:19:04 -0800

    Modified schema to match master

--- db/schema.rb
+++ db/schema.rb
@@ -24,13 +24,6 @@ ActiveRecord::Schema.define(version: 20140227010310) do
     t.date     "date"
   end
 
-  create_table "entries", force: true do |t|
-    t.boolean  "status"
-    t.integer  "habit_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
   create_table "friendships", force: true do |t|
     t.integer  "user_id"
     t.integer  "friend_id"
@@ -60,6 +53,7 @@ ActiveRecord::Schema.define(version: 20140227010310) do
   create_table "points", force: true do |t|
     t.integer  "user_id"
     t.integer  "pointable_id"
+    t.string   "pointable_type"
     t.integer  "action_id"
     t.datetime "created_at"
     t.datetime "updated_at"
