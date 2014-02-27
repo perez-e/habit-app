ede7006e06f3a053a29fb13ddf660905c5b64ec7
lalanikki <lalanikki@gmail.com>
2014-02-27 15:02:06 -0800

    new method returns json object of possible friends

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -8,9 +8,11 @@ class FriendshipsController < ApplicationController
   end
 
   def new
-    @user = current_user
-    results = search_matches(search)
-    @friendship = @user.friendships.build()
+    user = current_user
+    results = search_matches(params[:email])
+    respond_to do |f|
+      f.json { render json: results } 
+    end
   end
 
   def create
@@ -51,13 +53,22 @@ class FriendshipsController < ApplicationController
     users = User.all
     matches = []
     users.each do |user|
-      if /#{email}/.match(user.email)  
-        matches << user
+      if /#{email}/.match(user.email)
+        if user.profile
+          url = user.profile.profile_pic.url(:thumb)
+          list_info = { 
+            :name => (user.first_name + " " + user.last_name),
+            :pic_url => url
+           }
+        else
+          list_info = {
+            :name => user.first_name
+          }
+        end
+        matches << list_info
       end
-    # end
-    # respond_to do |f|
-    # f.json { render :json => {}}
     end
+    return matches
   end
 
 end
