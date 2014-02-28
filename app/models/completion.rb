6a126eb08e8daca66eb9e75b8d79548918816b4d
Eric Perez <ericricardoperez@gmail.com>
2014-02-28 02:40:40 -0800

    attached point to completions

--- app/models/completion.rb
+++ app/models/completion.rb
@@ -2,5 +2,5 @@ class Completion < ActiveRecord::Base
   belongs_to :habit, :foreign_key => "habit_id",
                      :class_name  => "Habit"
 
-  has_one :point, as: :pointable
+  has_one :point, as: :pointable, dependent: :destroy
 end
